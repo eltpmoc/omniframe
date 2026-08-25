4c61fd5a4e07dd0da8929176974f0d2dda6f948b
eltonalvesp <eltonalvesp@github.com>
2026-08-25 16:25:14 -0300

    feat: adiciona ponto de entrada unificado All-in-One (v1.2.1)

---
+++ src/js/omniframe.js
@@ -0,0 +1,33 @@
+/* ==========================================================================
+   OmniFrame v1.2 - All-in-One JS
+   Importa e inicializa todos os componentes do framework.
+   ========================================================================== */
+
+import { Alert } from './components/Alert.js';
+import { Carousel } from './components/Carousel.js';
+import { DataTable } from './components/DataTable.js';
+import { Dropdown } from './components/Dropdown.js';
+import { FAB } from './components/FAB.js';
+import { GlobalSearch } from './components/GlobalSearch.js';
+import { ListBox } from './components/ListBox.js';
+import { Media } from './components/Media.js';
+import { Modal } from './components/Modal.js';
+import { Sidebar } from './components/Sidebar.js';
+import { SmartSelect } from './components/SmartSelect.js';
+import { Tabs } from './components/Tabs.js';
+
+// Exportando para caso o dev queira instanciar via JS manualmente
+export {
+    Alert,
+    Carousel,
+    DataTable,
+    Dropdown,
+    FAB,
+    GlobalSearch,
+    ListBox,
+    Media,
+    Modal,
+    Sidebar,
+    SmartSelect,
+    Tabs
+};
